cd376475f32097e0e4f8d6497d95c8435bb317fb
Andrey <andreyacostar@gmail.com>
2024-07-09 21:22:19 -0600

    feat: Add brand signup component template and styles

---
+++ src/app/pages/auth/brand-signup/brand-signup.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-brand-signup',
+  standalone: true,
+  imports: [],
+  templateUrl: './brand-signup.component.html',
+  styleUrl: './brand-signup.component.scss'
+})
+export class BrandSignupComponent {
+
+}
