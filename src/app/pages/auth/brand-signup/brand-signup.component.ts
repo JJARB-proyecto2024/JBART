d638162245ba20d4f4380273ea839468ac1413c9
Andrey <andreyacostar@gmail.com>
2024-07-13 11:25:46 -0600

    feat: Update brand signup component to use legalDocuments instead of legalDocument

--- src/app/pages/auth/brand-signup/brand-signup.component.ts
+++ src/app/pages/auth/brand-signup/brand-signup.component.ts
@@ -21,6 +21,7 @@ export class BrandSignupComponent {
   @ViewChild('brandName') brandName!: NgModel;
   @ViewChild('legalRepresentativeName') legalRepresentativeName!: NgModel;
   @ViewChild('mainLocationAddress') mainLocationAddress!: NgModel;
+  @ViewChild('legalDocuments') legalDocuments!: NgModel;
   @ViewChild('brandCategory') brandCategories!: NgModel;
   @ViewChild('email') email!: NgModel;
   @ViewChild('password') password!: NgModel;
