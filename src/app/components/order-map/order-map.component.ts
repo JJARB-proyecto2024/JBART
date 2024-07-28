7dee8748c77571e9d8c7f423cf4744f2be487242
Maxwell Chinchilla Abarca <64172254+maxwell-abarca@users.noreply.github.com>
2024-07-27 23:17:55 -0600

    chore: added order-map component

---
+++ src/app/components/order-map/order-map.component.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit, SimpleChanges } from '@angular/core';
+import { AngularOpenlayersModule } from 'ng-openlayers';
+import { OrderService } from '../../services/order.service';
+
+@Component({
+  selector: 'app-order-map',
+  standalone: true,
+  imports: [AngularOpenlayersModule],
+  templateUrl: './order-map.component.html',
+  styleUrl: './order-map.component.scss'
+})
+export class OrderMapComponent implements OnInit {
+  public direction: String = '';
+  public orderService: OrderService = inject(OrderService);
+
+  ngOnInit(): void {
+    this.orderService.getAll();
+  }
+}
