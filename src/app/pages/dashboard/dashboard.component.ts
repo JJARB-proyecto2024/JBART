28419916df7993cf4f2a67362b388a39cf50e988
Burenam <burenam@ucenfotec.ac.cr>
2024-07-31 18:55:47 -0600

    fix: dashboard
    fix: dashboard

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ProductRecommendedListComponent } from '../../components/products-recomended/product-recommended-list/product-recommended-list.component';
+import { ProductService } from '../../services/product.service';
+import { LoaderComponent } from '../../components/loader/loader.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterLink,
+    LoaderComponent,
+    ProductRecommendedListComponent,],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit{
+  public productService: ProductService = inject(ProductService);
 
+  ngOnInit(): void {
+    this.productService.getAll();
+  }
 }
