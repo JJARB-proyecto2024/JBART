160555fd3dd564a4baed22bc4fac6c07ecde7a3d
Maxwell Chinchilla Abarca <64172254+maxwell-abarca@users.noreply.github.com>
2024-08-08 20:16:16 -0600

    chore: added notification template structure

--- src/app/interfaces/index.ts
+++ src/app/interfaces/index.ts
@@ -66,7 +66,7 @@ export interface IOtp {
 export interface IRateBrand {
   id?: number;
   rate?: number;
-  userBrand : IBrandUser;
+  userBrand: IBrandUser;
 }
 
 export interface IPayment {
@@ -105,10 +105,16 @@ export interface IProduct {
   updatedAt?: string;
 }
 
-export interface INotification {
+export interface INotificationTemplate {
   id?: number;
   title?: string;
   description?: string;
+  redirectLink?:string;
+}
+
+export interface INotification {
+  id?: number;
+  notificationTemplate?: INotificationTemplate;
   seen?: boolean;
   user?: IUser;
   redirectLink?: string;
