018c44c0ae7fe595cc3c492fbd8abad3231661b0
Maxwell Chinchilla Abarca <64172254+maxwell-abarca@users.noreply.github.com>
2024-08-12 01:08:12 -0600

    chore: added handleSeenAll to websocket notifications

--- src/app/components/notifications/notifications.component.ts
+++ src/app/components/notifications/notifications.component.ts
@@ -32,6 +32,8 @@ export class NotificationsComponent implements OnInit {
       (message) => {
         const notification: INotification = JSON.parse(message);
         this.notifications.push(notification);
+        console.log('notifications', this.notifications);
+        this.handleSeenAll();
       },
       (error) => {
         console.error(error);
