fa56b6e13ed76953ac2b15f7f4c59651b2da266b
Andrey <andreyacostar@gmail.com>
2024-08-19 18:37:38 -0600

    feat: remove swl.fire message when user logs in

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -48,14 +48,7 @@ export class LoginComponent {
     if (this.emailModel.valid && this.passwordModel.valid) {
       this.authService.login(this.loginForm).subscribe({
         next: () => {
-          Swal.fire({
-            title: 'Éxito',
-            text: 'Iniciaste sesión correctamente.',
-            icon: 'success',
-            confirmButtonText: 'Aceptar'
-          }).then(() => {
             this.router.navigateByUrl('/app/dashboard');
-          });
         },
         error: (err: any) => {
           if (err.error.errorMessage) {
